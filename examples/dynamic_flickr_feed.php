bb8b8a6675264608a122f775c5c6126b53ebff5e
jsor <jan@sorgalla.com>
2010-10-22 13:41:32 +0200

    Fix example

--- examples/dynamic_flickr_feed.php
+++ examples/dynamic_flickr_feed.php
@@ -3,7 +3,7 @@
 header('Content-type: application/json; charset: utf-'.'8');
 //header('Cache-Control: must-revalidate');
 
-$url = 'http://api.flickr.com/services/feeds/photos_public.gne?format=json';
+$url = 'http://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1';
 
 if (!empty($_REQUEST['tags'])) {
     $url .= '&tags=' . urlencode($_REQUEST['tags']);
