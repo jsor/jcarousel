f1b43e3a4b49619cf08af1240d901d44eda21857
jsor <jan@sorgalla.com>
2011-03-16 10:34:32 +0100

    Remove unused options

--- test/unit/jquery.jcarousel.js
+++ test/unit/jquery.jcarousel.js
@@ -2,14 +2,6 @@ $(function($){
 
     module("jquery.jcarousel");
 
-    test("init() sets data", function() {
-        expect(1);
-
-        $('#jcarousel1').jcarousel();
-
-        ok($('#jcarousel1').data('jcarousel'), '#jcarousel1');
-    });
-
     test("setup() sets vertical", function() {
         expect(4);
 
@@ -30,16 +22,6 @@ $(function($){
         equals(true, $('#jcarousel2').data('jcarousel').rtl, '#jcarousel2 (attribute: dir="rtl")');
         equals(true, $('#jcarousel3').data('jcarousel').rtl, '#jcarousel3 (parent attribute: dir="rtl")');
         equals(false, $('#jcarousel4').data('jcarousel').rtl, '#jcarousel4');
-    });
-
-    test("setup() sets circular", function() {
-        expect(2);
-
-        $('#jcarousel1').jcarousel();
-        $('#jcarousel2').jcarousel({wrap: 'circular'});
-
-        equals(false, $('#jcarousel1').data('jcarousel').circular, '#jcarousel1');
-        equals(true, $('#jcarousel2').data('jcarousel').circular, '#jcarousel2');
     });
 
 });
