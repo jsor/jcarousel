39a229cca55a3566e9f89b35b3c455802c3af151
jsor <jan@sorgalla.com>
2011-03-16 10:48:13 +0100

    Add more tests

--- test/unit/jquery.jcarousel.js
+++ test/unit/jquery.jcarousel.js
@@ -2,6 +2,55 @@ $(function($){
 
     module("jquery.jcarousel");
 
+    test("init() sets element", function() {
+        expect(2);
+
+        $('#jcarousel1').jcarousel();
+
+        ok($('#jcarousel1').data('jcarousel').element, '#jcarousel1 (element exists)');
+        ok($('#jcarousel1').data('jcarousel').element.jquery, '#jcarousel1 (element is jQuery object)');
+    });
+
+    test("init() sets options", function() {
+        expect(4);
+
+        $('#jcarousel1').jcarousel({wrap: 'custom', foo: 'bar'});
+
+        ok($('#jcarousel1').data('jcarousel').options.wrap, '#jcarousel1 (wrap exists)');
+        equals('custom', $('#jcarousel1').data('jcarousel').options.wrap, '#jcarousel1 (wrap equals)');
+
+        ok($('#jcarousel1').data('jcarousel').options.foo, '#jcarousel1 (foo exists)');
+        equals('bar', $('#jcarousel1').data('jcarousel').options.foo, '#jcarousel1 (foo equals)');
+    });
+
+    test("init() sets data", function() {
+        expect(1);
+
+        $('#jcarousel1').jcarousel();
+
+        ok($('#jcarousel1').data('jcarousel'), '#jcarousel1');
+    });
+
+    test("init() sets callbacks", function() {
+        expect(2);
+
+        $('#jcarousel1').jcarousel();
+
+        ok($('#jcarousel1').data('jcarousel').onWindowResize, '#jcarousel1 (onWindowResize)');
+        ok($('#jcarousel1').data('jcarousel').onAnimationComplete, '#jcarousel1 (onAnimationComplete)');
+    });
+
+    test("init() calls setup()", function() {
+        expect(1);
+
+        stop();
+
+        $('#jcarousel1').bind('jcarouselsetup', function() {
+            ok(true, "#jcarousel1");
+            start();
+        }).jcarousel();
+    });
+
     test("setup() sets vertical", function() {
         expect(4);
 
@@ -22,6 +71,16 @@ $(function($){
         equals(true, $('#jcarousel2').data('jcarousel').rtl, '#jcarousel2 (attribute: dir="rtl")');
         equals(true, $('#jcarousel3').data('jcarousel').rtl, '#jcarousel3 (parent attribute: dir="rtl")');
         equals(false, $('#jcarousel4').data('jcarousel').rtl, '#jcarousel4');
+    });
+
+    test("setup() sets circular", function() {
+        expect(2);
+
+        $('#jcarousel1').jcarousel();
+        $('#jcarousel2').jcarousel({wrap: 'circular'});
+
+        equals(false, $('#jcarousel1').data('jcarousel').circular, '#jcarousel1');
+        equals(true, $('#jcarousel2').data('jcarousel').circular, '#jcarousel2');
     });
 
 });
