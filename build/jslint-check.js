3fdc6f997ee27495be8bbcda65f81c8c0314ed5c
jsor <jan@sorgalla.com>
2011-04-27 14:07:41 +0200

    Allow multiple files for jslint target

--- build/jslint-check.js
+++ build/jslint-check.js
@@ -1,39 +1,48 @@
 load("build/jslint.js");
 
-var src = readFile(arguments[0]);
-if (!src){
-    print("Couldn't open file '" + arguments[0] + "'.");
-    quit(1);
-}
+var files = arguments[0].split(' ');
 
-JSLINT(src, {rhino: true, evil: true, forin: true, maxerr: 100});
+for (var j = 0, l = files.length; j < l; j++) {
 
-// All of the following are known issues that we think are 'ok'
-// (in contradiction with JSLint) more information here:
-// http://docs.jquery.com/JQuery_Core_Style_Guidelines
-var ok = {
-    "Expected an identifier and instead saw 'undefined' (a reserved word).": true,
-    "Use '===' to compare with 'null'.": true,
-    "Use '!==' to compare with 'null'.": true,
-    "Expected an assignment or function call and instead saw an expression.": true,
-    "Expected a 'break' statement before 'case'.": true,
-    "'e' is already defined.": true
-};
-
-var e = JSLINT.errors, found = 0, w;
-
-for (var i = 0; i < e.length; i++) {
-    w = e[i];
-
-    if (!ok[ w.reason ]) {
-        found++;
-        print("\n" + w.evidence + "\n");
-        print("  Problem at line " + w.line + " character " + w.character + ": " + w.reason);
+    var src = readFile(files[j]);
+    if (!src){
+        print("Couldn't open file '" + files[j] + "'.");
+        quit(1);
     }
-}
 
-if (found > 0) {
-    print( "\n" + found + " Error(s) found.\n" );
-} else {
-    print( "JSLint check passed.\n" );
+    print("Checking file '" + files[j] + "'...");
+
+    JSLINT(src, {rhino: true, evil: true, forin: true, maxerr: 100});
+
+    // All of the following are known issues that we think are 'ok'
+    // (in contradiction with JSLint) more information here:
+    // http://docs.jquery.com/JQuery_Core_Style_Guidelines
+    var ok = {
+        "Expected an identifier and instead saw 'undefined' (a reserved word).": true,
+        "Use '===' to compare with 'null'.": true,
+        "Use '!==' to compare with 'null'.": true,
+        "Expected an assignment or function call and instead saw an expression.": true,
+        "Expected a 'break' statement before 'case'.": true,
+        "'e' is already defined.": true
+    };
+
+    var e = JSLINT.errors, found = 0, w;
+
+    for (var i = 0; i < e.length; i++) {
+        w = e[i];
+
+        if (!ok[ w.reason ]) {
+            found++;
+            print("\n" + w.evidence + "\n");
+            print("  Problem at line " + w.line + " character " + w.character + ": " + w.reason);
+        }
+    }
+
+    if (found > 0) {
+        print( "\n" + found + " Error(s) found.\n" );
+    } else {
+        print( "JSLint check passed.\n" );
+    }
+
+    print("-----------------------\n");
 }
