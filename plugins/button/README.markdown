5553c993e5729023db99023f3122b167452f0420
Jan Sorgalla <jan@sorgalla.com>
2011-09-19 23:44:52 +0200

    Button plugin

---
+++ plugins/button/README.markdown
@@ -0,0 +1,15 @@
+jCarousel Button Plugin
+=======================
+
+The jCarousel Button Plugin provides Previous/Next buttons for jCarousel.
+
+Getting started
+---------------
+
+To use the jCarousel Button Plugin, include the source file right after the jCarousel core file:
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="/path/to/jquery.jcarousel.min.js"></script>
+    <script type="text/javascript" src="/path/to/jquery.jcarousel.button.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="/path/to/skin.css" />
+
---
+++ plugins/button/src/jquery.jcarousel.button.js
@@ -0,0 +1,99 @@
+/*!
+ * jCarousel Button Plugin v@VERSION
+ * http://sorgalla.com/jcarousel/
+ *
+ * Copyright 2011, Jan Sorgalla
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
+ * or GPL Version 2 (http://www.opensource.org/licenses/gpl-2.0.php) licenses.
+ *
+ * Date: @DATE
+ */
+(function($) {
+
+    $.jcarousel.create('jcarousel.button', {
+        options: {
+            scroll: 1,
+            event: 'click'
+        },
+        _init: function() {
+            var self = this;
+
+            this.carousel()
+                .bind('jcarouseldestroy', function() {
+                    self.destroy();
+                })
+                .bind('jcarouselreloadend jcarouselscrolltoend jcarouselscrollbyend', function() {
+                    self.reload();
+                });
+
+            this.reload();
+        },
+        reload: function() {
+            var carousel = this.carousel(),
+                scroll   = this.option('scroll');
+
+            this.element
+                .unbind('.' + this._event)
+                .bind(this.option('event') + '.' + this._event, function() {
+                    if ($.data(this, 'jcarousel-button-enabled')) {
+                        carousel.jcarousel('scrollBy', scroll);
+
+                    }
+                    return false;
+                });
+
+            var enabled = false,
+                instance,
+                wrap,
+                items,
+                size;
+
+            carousel.each(function() {
+                instance = $.data(this, 'jcarousel');
+                wrap     = instance.option('wrap');
+                items    = instance.items();
+                size     = items.size();
+
+                if (scroll > 0) {
+                    enabled = size > 0 &&
+                        ((wrap && wrap !== 'first') ||
+                         (items.filter(':jcarousel-item-last').index() < (size - 1)) ||
+                         (instance.tail && !instance.inTail)) ? true : false;
+                } else {
+                    enabled = size > 0 &&
+                        ((wrap && wrap !== 'last') ||
+                         (items.filter(':jcarousel-item-first').index() > 0) ||
+                         (instance.tail && instance.inTail)) ? true : false;
+                }
+
+                if (enabled) {
+                    return false;
+                }
+            });
+
+            if (this.element.data('jcarousel-button-enabled') !== enabled) {
+                this.element
+                    .data('jcarousel-button-enabled',  enabled)
+                    .data('jcarousel-button-disabled', !enabled)
+                    .trigger('jcarouselbutton' + (enabled ? 'enabled' : 'disabled'));
+            }
+
+            return this;
+        },
+        destroy: function() {
+            this.element
+                .removeData(':jcarousel-button-enabled')
+                .removeData(':jcarousel-button-disabled')
+                .unbind('.' + this._event);
+        }
+    });
+
+    $.expr.filters['jcarousel-button-enabled'] = function(elem) {
+        return !!$.data(elem, 'jcarousel-button-enabled');
+    };
+
+    $.expr.filters['jcarousel-button-disabled'] = function(elem) {
+        return !!$.data(elem, 'jcarousel-button-disabled');
+    };
+
+})(jQuery);
