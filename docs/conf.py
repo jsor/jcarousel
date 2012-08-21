e5fc570b21e84441b516c01e0654ad948279d615
Jan Sorgalla <jan@sorgalla.com>
2012-08-21 11:14:02 +0200

    Add GA code

--- docs/conf.py
+++ docs/conf.py
@@ -102,7 +102,10 @@ else:
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "googleanalytics_id": "UA-34232738-1",
+    "googleanalytics_path": "/jcarousel/"
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
