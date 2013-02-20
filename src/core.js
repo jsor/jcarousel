edb97e23c729ea2f0f55b2c8929ae118e4148ce6
Jan Sorgalla <jan@sorgalla.com>
2013-02-20 20:26:38 +0100

    Fix event triggering for delegated events (see #493)

--- src/core.js
+++ src/core.js
@@ -146,12 +146,22 @@
                 return this._carousel;
             },
             _trigger: function(type, element, data) {
-                var event = $.Event((type + '.' + pluginName).toLowerCase());
+                var event,
+                    defaultPrevented = false;
 
-                (element || this._element)
-                    .trigger(event, [this].concat(data || []));
+                data = [this].concat(data || []);
 
-                return !event.isDefaultPrevented();
+                (element || this._element).each(function() {
+                    event = $.Event((type + '.' + pluginName).toLowerCase());
+
+                    $(this).trigger(event, data);
+
+                    if (event.isDefaultPrevented()) {
+                        defaultPrevented = true;
+                    }
+                });
+
+                return !defaultPrevented;
             }
         };
     };
