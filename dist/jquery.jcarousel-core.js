0423b7cdaa6caeee6eea59891956a7b05fa73e8a
Jan Sorgalla <jan@sorgalla.com>
2013-08-20 09:20:23 +0200

    Update dist files

--- dist/jquery.jcarousel-core.js
+++ dist/jquery.jcarousel-core.js
@@ -1,4 +1,4 @@
-/*! jCarousel - v0.3.0-beta.5 - 2013-07-10
+/*! jCarousel - v0.3.0-beta.5 - 2013-08-20
 * http://sorgalla.com/jcarousel
 * Copyright (c) 2013 Jan Sorgalla; Licensed MIT */
 (function($) {
@@ -322,7 +322,8 @@
 
             this.lt = this.vertical ? 'top' : 'left';
 
-            // Force items reload
+            // Force list and items reload
+            this._list  = null;
             this._items = null;
 
             var item = this._target && this.index(this._target) >= 0 ?
--- dist/jquery.jcarousel-scrollintoview.js
+++ dist/jquery.jcarousel-scrollintoview.js
@@ -1,4 +1,4 @@
-/*! jCarousel - v0.3.0-beta.5 - 2013-07-10
+/*! jCarousel - v0.3.0-beta.5 - 2013-08-20
 * http://sorgalla.com/jcarousel
 * Copyright (c) 2013 Jan Sorgalla; Licensed MIT */
 (function($) {
