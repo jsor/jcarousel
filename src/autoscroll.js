c7e683f2e16bad5305ebcd9d564c5637976a84c2
Jan Sorgalla <jsorgalla@gmail.com>
2015-04-07 16:46:42 +0200

    Pause autoscrolling when document is hidden

--- src/autoscroll.js
+++ src/autoscroll.js
@@ -5,8 +5,26 @@
  *     core.js
  *     core_plugin.js
  */
-(function($) {
+(function($, document) {
     'use strict';
+
+    var hiddenProp,
+        visibilityChangeEvent,
+        visibilityChangeEventNames = {
+            hidden: 'visibilitychange',
+            mozHidden: 'mozvisibilitychange',
+            msHidden: 'msvisibilitychange',
+            webkitHidden: 'webkitvisibilitychange'
+        }
+    ;
+
+    $.each(visibilityChangeEventNames, function(key, val) {
+        if (typeof document[key] !== 'undefined') {
+            hiddenProp = key;
+            visibilityChangeEvent = val;
+            return false;
+        }
+    });
 
     $.jCarousel.plugin('jcarouselAutoscroll', {
         _options: {
@@ -15,6 +33,7 @@
             autostart: true
         },
         _timer: null,
+        _started: false,
         _init: function () {
             this.onDestroy = $.proxy(function() {
                 this._destroy();
@@ -22,23 +41,42 @@
                     .one('jcarousel:createend', $.proxy(this._create, this));
             }, this);
 
-            this.onAnimateEnd = $.proxy(this.start, this);
+            this.onAnimateEnd = $.proxy(this._start, this);
+
+            this.onVisibilityChange = $.proxy(function() {
+                if (document[hiddenProp]) {
+                    this._stop();
+                } else {
+                    this._start();
+                }
+            }, this);
         },
         _create: function() {
             this.carousel()
                 .one('jcarousel:destroy', this.onDestroy);
+
+            $(document)
+                .on(visibilityChangeEvent, this.onVisibilityChange);
 
             if (this.options('autostart')) {
                 this.start();
             }
         },
         _destroy: function() {
-            this.stop();
+            this._stop();
+
             this.carousel()
                 .off('jcarousel:destroy', this.onDestroy);
+
+            $(document)
+                .off(visibilityChangeEvent, this.onVisibilityChange);
         },
-        start: function() {
-            this.stop();
+        _start: function() {
+            this._stop();
+
+            if (!this._started) {
+                return;
+            }
 
             this.carousel()
                 .one('jcarousel:animateend', this.onAnimateEnd);
@@ -49,7 +87,7 @@
 
             return this;
         },
-        stop: function() {
+        _stop: function() {
             if (this._timer) {
                 this._timer = clearTimeout(this._timer);
             }
@@ -58,6 +96,18 @@
                 .off('jcarousel:animateend', this.onAnimateEnd);
 
             return this;
+        },
+        start: function() {
+            this._started = true;
+            this._start();
+
+            return this;
+        },
+        stop: function() {
+            this._started = false;
+            this._stop();
+
+            return this;
         }
     });
-}(jQuery));
+}(jQuery, document));
