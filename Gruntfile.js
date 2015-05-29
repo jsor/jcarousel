c20615f7f22ff9946f6c6181823469da8440a658
Jan Sorgalla <jsorgalla@gmail.com>
2015-05-29 22:45:50 +0200

    Correct license field in package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
             '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
             '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
             '* Copyright (c) 2006-<%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+            ' Licensed <%= pkg.license %> */\n',
         clean: {
             files: ['dist/*.js']
         },
