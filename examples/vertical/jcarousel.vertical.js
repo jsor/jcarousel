47e85a2aa7c820cbc73479769254d7c2e772294d
Evgeniy Pelmenev <g.snake345@gmail.com>
2015-12-20 02:59:34 +0200

    add vertical carousel example

---
+++ examples/vertical/jcarousel.vertical.js
@@ -0,0 +1,39 @@
+(function($) {
+    $(function() {
+        $('.jcarousel')
+            .jcarousel({
+                vertical: true
+            });
+
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .jcarouselPagination();
+    });
+})(jQuery);
